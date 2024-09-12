de322c382a352fd8ef2fd8c4db726fef02374692
Abdallah Elshenawy <abdallah.elshenawy099@gmail.com>
2024-09-12 22:38:24 +0300

    New version

--- main.py
+++ main.py
@@ -1,105 +1,84 @@
 import random
 words = ["ali", "karem", "isaq", "kamel", "nader"]
- 
-# choosing a random word
 rand_word = random.choice(words)
-
-# the spaces of the rand word
-displays = ["_"] * len(rand_word)
-
-# the list of the exist letters
-exist_list = []
-
-hangman = ["""          
-   +---+
+displays = ['_'] * len(rand_word)
+print(" ".join(displays))
+Hangman = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
   |   |
   O   |
- /|\\  |
- / \\  |
       |
-=========
-""","""          
-   +---+
+      |
+      |
+=========''', '''
+  +---+
   |   |
   O   |
- /|\\  |
- /    |
-      |
-=========
-""", """      
-   +---+
   |   |
-  O   |
- /|\\  |
       |
       |
-=========
-""", """
-   +---+
+=========''', '''
+  +---+
   |   |
   O   |
  /|   |
       |
       |
-=========
-""", """
-   +---+
+=========''', '''
+  +---+
   |   |
   O   |
+ /|\\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
  /    |
       |
-      |
-=========
-""", """
-   +---+
+=========''', '''
+  +---+
   |   |
   O   |
+ /|\\  |
+ / \\  |
       |
-      |
-      |
-=========
-""", """
-   +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-"""]
+=========''']
+
 chances = 6
-print(" ".join(displays))
-print(hangman[6])
-while ("_" in displays) and (chances != 0):
-   guessed = input("Please guess a letter: ")
+print(Hangman[6 - chances])
+exist_list = []
 
-   # did the user guess the letter before
-   if guessed in exist_list:
-      print("\nYou already guessed that. Try again.")
-      print(f"You have {chances} more tries.")
-      continue
-
-   # store the guessed letter in the exist_list
-   exist_list.append(guessed)
-
-   if (guessed not in rand_word):
+while ('_' in displays and chances > 0):
+   guessed = input("Guess a letter: ").lower()
+   if (guessed in exist_list):
+      print("You guessed this letter before")
+   elif (guessed not in rand_word):
       chances -= 1
-      print(hangman[chances])
+      print(Hangman[6 - chances])
    else:
       for index in range(len(rand_word)):
          if (guessed == rand_word[index]):
-            displays[index] = guessed
-            break
-      
-   print(" ".join(displays))
-   print(f"You have {chances} more tries.")
-if chances == 0:
-   print("""
-   YOU LOSE ! 
-   """)
-   print(hangman[0])
+            displays[index] = rand_word[index]
 
+   print(" ".join(displays))
+   exist_list.append(guessed)
+   print(f"You have only {chances} more tries\n")
+
+if (chances == 0):
+   print("""   
+   *** GAME OVER ***
+""")
 else:
    print("""
-   YOU WIN !
-   """)
+   *** You Win !! ***
+""")
